a8bdc57b86248be6fc7a0c5e0d578d859e222b7f
Jeppe Hyldgaard Olesen <jeppe@gejststudio.com>
2018-12-14 17:24:15 +0100

    Updates

---
+++ precache-manifest.3f77a40c0ee0b79ff3bbe7ffa98ddadd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "93a926fd2b969cda7674",
+    "url": "/livestats-comments/static/js/runtime~main.93a926fd.js"
+  },
+  {
+    "revision": "a10ea5ebdbb2a4440351",
+    "url": "/livestats-comments/static/js/main.a10ea5eb.chunk.js"
+  },
+  {
+    "revision": "35531d326421998f8603",
+    "url": "/livestats-comments/static/js/1.35531d32.chunk.js"
+  },
+  {
+    "revision": "0e8c2d5da2d96de8c8bf7efc8a96e859",
+    "url": "/livestats-comments/index.html"
+  }
+];
