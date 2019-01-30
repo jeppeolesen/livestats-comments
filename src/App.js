67c7e55d573f5cb879312e53599485a8325a9865
Jeppe Hyldgaard Olesen <jeppe@gejststudio.com>
2019-01-30 18:18:01 +0100

    Provide theme as a prop

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
-import styled, { createGlobalStyle } from 'styled-components'
+import styled, { createGlobalStyle, ThemeProvider } from 'styled-components'
 import global from 'styles/global'
 import { BaseCSS } from 'styled-bootstrap-grid'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import theme from 'styles/theme'
 
 import Navigation from 'components/Navigation'
 import PeriodStats from 'components/PeriodStats'
 import PlayerProfile from 'components/PlayerProfile'
 import PlayerComparison from 'components/PlayerComparison'
 import TeamComparison from 'components/TeamComparison'
+import Landing from 'components/Landing'
 
 const Wrapper = styled.div`
   background: #F0F0F0;
@@ -23,19 +26,22 @@ const GlobalStyles = createGlobalStyle`
 export default class App extends Component {
   render() {
     return (
-      <Router>
-        <Wrapper>
-          <Navigation />
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Wrapper>
+            <Navigation />
 
-          <Route path="/period-stats" component={PeriodStats} />
-          <Route path="/player-profile" component={PlayerProfile} />
-          <Route path="/player-comparison" component={PlayerComparison} />
-          <Route path="/team-comparison" component={TeamComparison} />
-          
-          <BaseCSS />
-          <GlobalStyles />
-        </Wrapper>
-      </Router>
+            <Route path="/" exact component={Landing} />
+            <Route path="/period-stats" component={PeriodStats} />
+            <Route path="/player-profile" component={PlayerProfile} />
+            <Route path="/player-comparison" component={PlayerComparison} />
+            <Route path="/team-comparison" component={TeamComparison} />
+            
+            <BaseCSS />
+            <GlobalStyles />
+          </Wrapper>
+        </Router>
+      </ThemeProvider>
     );
   }
 }
